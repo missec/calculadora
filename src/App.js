6db4227468cb99b064256b458ff7b79268233a54
Misserine <gabrielrocha@Mac-mini-de-Gabriel.local>
2020-03-14 11:32:59 -0300

    :sparkles: continuando construccion de la calculadora

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
 import React from "react";
 
-const Display = ({valor, ...props}) => {
+const styles = {
+    divDisplay: {
+        border: "1px solid",
+        margin: 2,
+        padding: 20,
+        backgroundColor: "#eeeeee",
+        textAlign: "right"
+    },
+    divBox: {
+        border: "1px solid",
+        margin: 2,
+        display: "inline-flex",
+        paddingBottom: 10,
+        paddingTop: 10,
+        width: "23.3%",
+        backgroundColor: "lightGray",
+        justifyContent: "center"
+    }
+};
+
+const Display = ({label, ...props}) => {
     // otra forma de asignacion por destructuring en la declaracion
-    // de la funcion: const Display = ({valor, ...props}) => {
-    // let {valor, ...rest} = props; //Asignación por destructuring
-    if (typeof valor === "undefined") {
-        valor = 0;
+    // de la funcion: const Display = ({label, ...props}) => {
+    // let {label, ...rest} = props; //Asignación por destructuring
+    console.log({label});
+    if (typeof label === "undefined") {
+        label = 0;
     }
 
     return (
-        <div style={{border: "1px solid", margin: 2, padding: 20, backgroundColor: "#eeeeee", textAlign: "right", ...props.style}}>
-            <h1>{valor}</h1>
+        <div style={{...styles.divDisplay, ...props.style}}>
+            <h1>{label}</h1>
         </div>
     );
 };
 
-const Box = () => {
-    let n = ["C", "( )", "%", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, ".", "="];
-    return n.map((num) => (
-        <div key={num} style={{border: "1px solid", margin: 2, display: "inline-flex", paddingBottom: 10, paddingTop: 10, width: "23.3%", backgroundColor: "lightGray", justifyContent: "center"}}>
-            <h2>{num}</h2>
+const Box = ({callBack}) => {
+    const teclas = ["C", "( )", "%", "/", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, ".", "="];
+
+    return teclas.map((tecla) => (
+        <div key={tecla} style={styles.divBox} onClick={() => callBack(tecla)}>
+            <h2>{tecla}</h2>
         </div>
     ));
 };
 
 // const Operators = () => {};
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            display: "",
+            previous: null
+        };
+    }
+
+    processar(botao) {
+        let {display, previous} = this.state;
+
+        switch (botao) {
+            case "+":
+                previous = display;
+                display = "";
+                break;
+            case "=":
+                display = parseInt(display);
+                break;
+            default:
+                break;
+        }
+        // if (this.isNumber(botao)) {
+        //     total = total;
+        // }
+
+        this.setState({
+            display: display + botao
+        });
+    }
+
+    render() {
+        const {total} = this.state;
+
+        return (
+            <div style={{width: 390, fontFamily: "sans-serif", margin: "7% auto", textAlign: "center"}}>
+                <h1>Calculadora</h1>
+                <div style={{padding: 2, border: "1px solid", width: 384}}>
+                    <Display label={total} />
+                    <Box callBack={(tecla) => this.processar(tecla)} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+/*creada como una funcion
 const App = () => {
     return (
         <div style={{width: 390, fontFamily: "sans-serif", margin: "7% auto", textAlign: "center"}}>
@@ -36,5 +109,4 @@ const App = () => {
             </div>
         </div>
     );
-};
-export default App;
+};*/
